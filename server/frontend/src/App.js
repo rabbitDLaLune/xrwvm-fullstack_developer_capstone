a67f139051b3bd9a60eb1ef3d9363b085c182eb9
khairul <dearkai311@gmail.com>
2026-06-17 02:18:05 -0400

    Registration added

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,13 +1,14 @@
-import LoginPanel from "./components/Login/Login"
-import RegisterPanel from "./components/Register/Register"
+import LoginPanel from "./components/Login/Login";
+import RegisterPanel from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-      <Route path="/login" element={<RegisterPanel />} />
+      <Route path="/register" element={<RegisterPanel />} />
     </Routes>
   );
 }
+
 export default App;
